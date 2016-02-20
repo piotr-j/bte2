d1d9bf2722bd96aec59c1d163dab03a38382b85d
EvilEntity <me@piotrjastrzebski.io>
2016-02-20 21:39:38 +0100

    cleaner api for editor window

--- bte-window/src/io/piotrjastrzebski/bte/window/AIEditorWindow.java
+++ bte-window/src/io/piotrjastrzebski/bte/window/AIEditorWindow.java
@@ -1,6 +1,5 @@
 package io.piotrjastrzebski.bte.window;
 
-import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
@@ -8,38 +7,60 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Window;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3WindowAdapter;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3WindowConfiguration;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import io.piotrjastrzebski.bte.AIEditor;
 
 /**
  * Simple util class that will open a new Lwjgl3 window with editor in it
  *
+ * Usage:
+ * <code>
+ * AIEditorWindow window = new AIEditorWindow(aiEditor);
+ * window.open();
+ * </code>
  * Created by EvilEntity on 19/02/2016.
  */
 public class AIEditorWindow {
 	private static final String TAG = AIEditorWindow.class.getSimpleName();
+	private final AIEditor editor;
+	private WindowListener listener;
 	private Lwjgl3Window editorWindow;
 	private Lwjgl3WindowConfiguration editorWindowConfig;
 	private ApplicationListener editorWindowListener;
 
 	/**
-	 * Open new window for given editor
+	 *
 	 * @param editor editor for window
+	 */
+	public AIEditorWindow (AIEditor editor) {
+		this.editor = editor;
+	}
+
+	/**
+	 * set listener for window events
+	 * @param listener listenr for window events
+	 */
+	public void setListener (WindowListener listener) {
+		this.listener = listener;
+	}
+
+	/**
+	 * Open new window for given editor
 	 * @return if window opened successfully
 	 */
-	public boolean open (final AIEditor editor) {
-		return open(-1, -1, 800, 600, editor, null);
+	public boolean open () {
+		return open(-1, -1, 800, 600);
 	}
 
 	/**
 	 * Open new window for given editor
 	 * @param width width of the window
 	 * @param height height of the window
-	 * @param editor editor for window
 	 * @return if window opened successfully
 	 */
-	public boolean open (int width, int height, final AIEditor editor) {
-		return open(-1, -1, width, height, editor, null);
+	public boolean open (int width, int height) {
+		return open(-1, -1, width, height);
 	}
 
 	/**
@@ -48,24 +69,9 @@ public class AIEditorWindow {
 	 * @param y y position of the window
 	 * @param width width of the window
 	 * @param height height of the window
-	 * @param editor editor for window
 	 * @return if window opened successfully
 	 */
-	public boolean open (int x, int y, int width, int height, final AIEditor editor) {
-		return open(x, y, width, height, editor, null);
-	}
-
-	/**
-	 * Open new window for given editor
-	 * @param x x position of the window
-	 * @param y y position of the window
-	 * @param width width of the window
-	 * @param height height of the window
-	 * @param editor editor for window
-	 * @param listener listener for window events
-	 * @return if window opened successfully
-	 */
-	public boolean open (int x, int y, int width, int height, final AIEditor editor, final WindowListener listener) {
+	public boolean open (int x, int y, final int width, int height) {
 		if (!(Gdx.app instanceof Lwjgl3Application)) {
 			Gdx.app.error(TAG, "MultiWindow is supported only in Lwjgl3 backend!");
 			return false;
@@ -74,6 +80,8 @@ public class AIEditorWindow {
 			editorWindowConfig = new Lwjgl3WindowConfiguration();
 			editorWindowConfig.setWindowListener(new Lwjgl3WindowAdapter(){
 				@Override public boolean windowIsClosing () {
+					Window window = editor.getWindow(false);
+					window.setMovable(true);
 					if (listener != null) {
 						listener.onClose();
 					}
@@ -87,7 +95,9 @@ public class AIEditorWindow {
 				com.badlogic.gdx.scenes.scene2d.Stage stage;
 				@Override public void create () {
 					stage = new com.badlogic.gdx.scenes.scene2d.Stage(new ScreenViewport());
-					stage.addActor(editor.getWindow(false));
+					Window window = editor.getWindow(false);
+					window.setMovable(false);
+					stage.addActor(window);
 					Gdx.input.setInputProcessor(stage);
 				}
 
@@ -145,7 +155,8 @@ public class AIEditorWindow {
 		 */
 		void onClose();
 	}
+
 	public static void main (String[] args) {
-		System.out.println("Hi");
+		System.out.println("Dummy main, move along! Do we even need this? probably not!");
 	}
 }
