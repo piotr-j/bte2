6c266747a2ecb2c97770e326dca4611da7972451
piotr-j <me@piotrjastrzebski.io>
2016-02-27 13:52:27 +0100

    comments, neat!

--- bte/src/io/piotrjastrzebski/bte/TaskInjector.java
+++ bte/src/io/piotrjastrzebski/bte/TaskInjector.java
@@ -3,8 +3,16 @@ package io.piotrjastrzebski.bte;
 import com.badlogic.gdx.ai.btree.Task;
 
 /**
+ * TaskInjector that should inject dependencies into {@link Task}s and its children if any
+ *
+ * @see {@link AIEditor#setTaskInjector(TaskInjector)}
+ *
  * Created by PiotrJ on 26/02/16.
  */
 public interface TaskInjector {
+	/**
+	 * Inject dependencies into given {@link Task} and its children
+	 * @param task task to inject
+	 */
 	void inject(Task task);
 }
